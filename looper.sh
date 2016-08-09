5b59d8ae89c16537e743bc5c786e8ad9fa066454
Eric Price <ecprice@mit.edu>
2016-08-09 11:31:41 -0700

    edit

--- looper.sh
+++ looper.sh
@@ -1,5 +1,5 @@
 sh synclogs >> /tmp/synclogs.log
 echo '\nsynced\n'
-python get_status.py > /tmp/cur_status
+python3 get_status.py > /tmp/cur_status
 echo '\n-------------------' `date` $'---------------\n'
 cat /tmp/cur_status
